b0c2ef7d1e5d206a39768039a6ad89db7b8796cb
Gabriel <gabreu954@outlook.com>
2022-05-25 18:28:29 -0300

    Adição de rotas na agenda

--- controllers/agenda.js
+++ controllers/agenda.js
@@ -7,6 +7,30 @@ const controller = {
       .then((result) => res.status(200).json(result))
       .catch((err) => res.status(400).json({ error: err.message }));
   },
+
+  getProfissional(req, res) {
+    if (req.body.data == undefined) {
+      agendaRepostory
+        .findAll({
+          where: {
+            profissional_id: req.params.profissional_id,
+          },
+        })
+        .then((result) => res.status(200).json(result))
+        .catch((err) => res.status(400).json({ error: err.message }));
+    } else {
+      agendaRepostory
+        .findAll({
+          where: {
+            profissional_id: req.params.profissional_id,
+            data: req.body.data,
+          },
+        })
+        .then((result) => res.status(200).json(result))
+        .catch((err) => res.status(400).json({ error: err.message }));
+    }
+  },
+
   post(req, res) {
     agendaRepostory
       .create({
@@ -16,6 +40,24 @@ const controller = {
         solicitacao_id: req.body.solicitacao_id,
       })
       .then((result) => res.status(201).json(result))
+      .catch((err) => res.status(400).json({ error: err.message }));
+  },
+  patch(req, res) {
+    agendaRepostory
+      .update(
+        {
+          data: req.body.data,
+          hora: req.body.hora,
+        },
+        { where: { solicitacao_id: req.params.solicitacao_id } }
+      )
+      .then(() => res.status(204).send())
+      .catch((err) => res.status(400).json({ error: err.message }));
+  },
+  delete(req, res) {
+    agendaRepostory
+      .destroy({ where: { solicitacao_id: req.params.solicitacao_id } })
+      .then((result) => res.status(200).json(result))
       .catch((err) => res.status(400).json({ error: err.message }));
   },
 };
--- routes/agenda.js
+++ routes/agenda.js
@@ -5,7 +5,15 @@ const routeName = "/agenda";
 const routes = (app) => {
 
     app.route(`/v1${routeName}`).get(controllerAgenda.get);
+
     app.route(`/v1${routeName}`).post(controllerAgenda.post);
+
+    app.route(`/v1${routeName}/profissional/:profissional_id`).get(controllerAgenda.getProfissional)
+
+    app.route(`/v1${routeName}/:solicitacao_id`).patch(controllerAgenda.patch);
+
+    app.route(`/v1${routeName}/:solicitacao_id`).delete(controllerAgenda.delete);
+    
 
 }
 
